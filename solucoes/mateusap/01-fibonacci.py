c2fc2b612084b0df2d39ce253c057aaa1c9482ca
mateusap <ma.aparecido.silva@hotmail.com>
2023-03-03 18:23:55 -0300

    Update 01-fibonacci.py

--- solucoes/mateusap/01-fibonacci.py
+++ solucoes/mateusap/01-fibonacci.py
@@ -4,7 +4,7 @@ def fibonacci (e: int):
     if e == 0 or e == 1:
         return "1" #coloquei o número 1 pra não retornar a lista vazia
     elif e <= 0:
-        return "Fibonacci não trabalha com números negativos
+        return "Fibonacci não trabalha com números negativos"
     
     sequencia = [1,1] #valores iniciais da sequencia seguindo o exemplo usando 1 e 1 ao invés de 0 e 1
     atual = 1

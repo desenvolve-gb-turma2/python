3781308aa05681fb00cbd91c22219e6ffd1d170e
mateusap <ma.aparecido.silva@hotmail.com>
2023-03-02 23:19:25 -0300

    Fibonacci

---
+++ solucoes/mateusap/01-fibonacci.py
@@ -0,0 +1,20 @@
+def fibonacci (e: int):
+    sequencia = []
+    
+    if e == 0 or e == 1:
+        return "1" #coloquei o número 1 pra não retornar a lista vazia
+    
+    sequencia = [1,1] #valores iniciais da sequencia seguindo o exemplo usando 1 e 1 ao invés de 0 e 1
+    atual = 1
+    anterior = 1
+
+    for i in range (2, e):
+        proximo = atual + anterior
+        sequencia.append(proximo)
+        anterior = atual
+        atual = proximo
+
+    return sequencia
+
+#seq = fibonacci(7) teste da função pra ver se estava funcionando
+#print (seq)

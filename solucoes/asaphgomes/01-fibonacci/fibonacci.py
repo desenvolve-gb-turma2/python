011bb3be18dffa53e807bd60c28988d52e3c7eff
Asaph Gomes <103325830+asaphgomes@users.noreply.github.com>
2023-03-23 17:09:34 -0300

    Inclui exceções no código
    Decidi incluir exceções no código para validar entradas.

--- solucoes/asaphgomes/01-fibonacci/fibonacci.py
+++ solucoes/asaphgomes/01-fibonacci/fibonacci.py
@@ -1,11 +1,21 @@
 def fibonacci ():
-  n = int(input('Quantos números terá a sequencia fibonacci? '))
-  fibonacci = [0,1]
-
-  for i in range (n-2):
-    fibonacci.append((fibonacci [-1]) + (fibonacci [-2]))
-  
-  return fibonacci
-  #return ", ".join(str(numero) for numero in fibonacci)
-
+  try:
+    
+    n = int(input('Quantos números terá a sequencia fibonacci? '))
+    fibonacci = [0,1]
+    
+    if n < 0:
+      return 'Digite um número inteiro positivo!'
+    elif n == 0:
+      return []
+    elif n == 1:
+      return [0]
+    elif n >= 2:
+      for i in range (n-2):
+        fibonacci.append((fibonacci [-1]) + (fibonacci [-2]))
+    return fibonacci
+    
+  except ValueError: 
+    return 'Digite um número inteiro positivo!'
+    
 print(fibonacci())
